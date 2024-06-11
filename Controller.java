535347573b2509b16d4922e2969f2cdbaead1704
estebanmrn <luisestebanmc@ufps.edu.co>
2024-06-11 18:25:19 -0500

    Controller.java

--- Controller.java
+++ Controller.java
@@ -1,12 +1,19 @@
 import java.net.URL;
+import javafx.application.Platform;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ListView;
+
 
 public class Controller {
+    public Controller(){
+    this.veterinaria = new Veterinaria();
+}
 
     @FXML
     private ResourceBundle resources;
@@ -55,6 +62,88 @@ public class Controller {
 
     @FXML
     private TextField txtRaza;
+    
+    private Veterinaria veterinaria;
+    private ListView<String> listMascotas;
+    private String nombreMascota, especieMascota, razaMascota;
+    private int edadMascota;
+    private Mascota mascota;
+    
+
+
+    @FXML
+    void edadIn(ActionEvent event) {
+        edadMascota = Integer.parseInt(txtEdad.getText());
+
+
+    }
+
+    @FXML
+    void especieIn(ActionEvent event) {
+        especieMascota = txtEspecie.getText();
+
+    }
+
+    @FXML
+    void mostrarMascotas(ActionEvent event) {
+        areaListado.clear();
+        for (Mascota mascota : veterinaria.getMascotas()){
+            areaListado.appendText(mascota.getNombre() + "\n");
+        }
+    }
+
+    @FXML
+    void nombreIn(ActionEvent event) {
+        nombreMascota = txtNombre.getText();
+    }
+
+    @FXML
+    void razaIn(ActionEvent event) {
+        razaMascota = txtRaza.getText();
+
+    }
+
+    @FXML
+    void registrarMasc(ActionEvent event) {
+        try {
+            String nombre = txtNombre.getText();
+            String raza = txtRaza.getText();
+            String especie = txtEspecie.getText();
+            int edad = Integer.parseInt(txtEdad.getText());
+            
+            if (nombre.isEmpty() || especie.isEmpty() || raza.isEmpty() || edad <= 0){
+                System.out.println ("Por favor, complete todos los datos");
+                return;
+            }
+            Mascota nuevaMascota = new Mascota (nombre, especie, edad, raza);
+            veterinaria.agregarMascota(nuevaMascota);
+            
+            //limpiar los campos despues de registrar
+            txtNombre.clear();
+            txtRaza.clear();
+            txtEspecie.clear();
+            txtEdad.clear();
+            
+            System.out.println ("Mascota registrada exitosamente");
+        }
+        catch (NumberFormatException e){
+            System.out.println ("Por favor, ingrese un número válido para la edad");
+        }
+        }
+        
+
+    @FXML
+    void salir(ActionEvent event) {
+        Platform.exit();
+
+    }
+    
+    public void startGUI(){
+        txtNombre.setText(mascota.getNombre());
+        txtEspecie.setText(mascota.getEspecie());
+        txtRaza.setText(mascota.getRaza());
+        txtEdad.setText(String.valueOf(mascota.getEdad()));
+    }
 
     @FXML
     void initialize() {
@@ -72,6 +161,8 @@ public class Controller {
         assert txtEspecie != null : "fx:id=\"txtEspecie\" was not injected: check your FXML file 'Veterinaria.fxml'.";
         assert txtNombre != null : "fx:id=\"txtNombre\" was not injected: check your FXML file 'Veterinaria.fxml'.";
         assert txtRaza != null : "fx:id=\"txtRaza\" was not injected: check your FXML file 'Veterinaria.fxml'.";
+        
+        
 
     }
 
